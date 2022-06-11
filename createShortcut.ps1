c3982c121e7207cc019a80e40085b8e4c88ecb03
Jf0-2 <102773107+Jf0-2@users.noreply.github.com>
2022-06-10 19:56:49 -0600

    Add files via upload

---
+++ createShortcut.ps1
@@ -0,0 +1,7 @@
+$startupFolder = "C:\Users\$Env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$saveFolder = "C:\Users\$Env:username\goose"
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$startupFolder\DesktopGoose.lnk")
+$Shortcut.TargetPath = "$saveFolder\goose\DesktopGoose\GooseDesktop.exe"
+$Shortcut.Save()
