11e288c86d9a22d9e2571f85e36a870974225974
Jf0-2 <102773107+Jf0-2@users.noreply.github.com>
2022-06-10 21:43:39 -0600

    Update createShortcut.ps1

--- createShortcut.ps1
+++ createShortcut.ps1
@@ -1,7 +1,3 @@
-$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
-add-type -name win -member $t -namespace native
-[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
-
 $startupFolder = "C:\Users\$Env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $saveFolder = "C:\Users\$Env:username\goose"
 
