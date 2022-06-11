a83566762457bc3ff786834d383c0b2855be7033
Jf0-2 <102773107+Jf0-2@users.noreply.github.com>
2022-06-11 10:34:47 -0600

    Update removeGoose.ps1

--- removeGoose.ps1
+++ removeGoose.ps1
@@ -1,9 +1,9 @@
 $startupFolder = "C:\Users\$Env:username\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 $saveFolder = "C:\Users\$Env:username\goose"
 
-Stop-Process -Name "Desktop Goose"
+Stop-Process -Name "GooseDesktop"
 
-Remove-Item $saveFolder
 Remove-Item "$startupFolder\DesktopGoose.lnk"
+&cmd.exe /c rd /s /q $saveFolder
 
-Write-Host -NoNewLine 'Removed Desktop Goose.'; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+Write-Host -NoNewLine 'Removed Desktop Goose; You can delete this folder when needed.'; $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
